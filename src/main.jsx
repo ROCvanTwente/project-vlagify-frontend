c5d9930203a6bc9a67ea9c424e3241fa59a4aea3
Sem Christenhusz <1357767@student.rocvantwente.nl>
2026-03-18 09:54:54 +0100

    Created home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { LocalizationProvider } from './context/LocalizationContext'
 import '../i18n/i18n.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <LocalizationProvider>
+        <App />
+      </LocalizationProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
